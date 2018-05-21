1eb4a08c31408769ac2983a89e27ca9c8a89ca49
KovtunIvan <34392496+KovtunIvan@users.noreply.github.com>
2018-05-21 09:25:31 +0300

    first commit

---
+++ components/element.random/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['T_IBLOCK_DESC_CAT_LIST'] = "Показ случайного товара с заданным св-вом";
+$MESS ['T_IBLOCK_DESC_CAT_DESC'] = "Показывает случайный элемент";
+$MESS ['T_IBLOCK_DESC_CAT'] = "Расширения каталога";
+?>
